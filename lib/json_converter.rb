32f0f7ca27f34c98cdf421356708ba60b02d686e
Devan <devan@blaze.com.au>
2019-02-11 21:02:23 +0930

    Added Float as valid conversion class

--- lib/json_converter.rb
+++ lib/json_converter.rb
@@ -72,7 +72,7 @@ class JsonConverter
 
   # Recursively flatten a hash (or array of hashes)
   def flatten(target, path='')
-    scalars = [String, Integer, Fixnum, FalseClass, TrueClass]
+    scalars = [String, Integer, Fixnum, Float, FalseClass, TrueClass]
     columns = {}
 
     if [Hash, Array].include? target.class
